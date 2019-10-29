ed9a5ad381b919babe8c06ab897b99735d6ea068
Reedo0910 <sea.sand@live.cn>
2019-10-29 00:11:38 -0400

    修复样式

--- pages/index.vue
+++ pages/index.vue
@@ -24,7 +24,8 @@
       <h1>科学ADV中文Wiki</h1>
       <p>
         这是一个关于
-        <em>科学ADV系列</em>的维基百科
+        <strong>科学ADV系列</strong>
+        的维基百科
       </p>
       <p>目前已有2个页面</p>
     </header>
